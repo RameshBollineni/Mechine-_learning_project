a29428044318557d6e9b7b1b41f279f363f6ea5a
Ramesh Bollineni <bollineniramesh.informatica@gamil.com>
2022-06-18 15:14:19 +0530

    message chnges

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask import Flask
 import sys
 from housing.logger import logging
-from housing.expection import HousingException
-app = Flask(__name__)
+from housing.exception import HousingException
+app=Flask(__name__)
+
 
 @app.route("/",methods=['GET','POST'])
-def hello():
+def index():
     try:
         raise Exception("We are testing custom exception")
     except Exception as e:
         housing = HousingException(e,sys)
         logging.info(housing.error_message)
         logging.info("We are testing logging module")
-    return "Hello world"
+    return "CI CD pipeline has been established."
 
-if __name__ == "__main__":
-    app.run()
+
+if __name__=="__main__":
+    app.run(debug=True)
