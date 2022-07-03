02ce679f13dbeab30afa5784f668ec9038a03468
Ramesh Bollineni <bollineniramesh.informatica@gamil.com>
2022-07-03 11:43:59 +0530

    updated

--- housing/constant/__init__.py
+++ housing/constant/__init__.py
@@ -1,3 +1,4 @@
+
 import os
 from datetime import datetime
 ROOT_DIR = os.getcwd()  #to get current working directory
@@ -27,3 +28,13 @@ DATA_INGESTION_TGZ_DOWNLOAD_DIR_KEY = "tgz_download_dir"
 DATA_INGESTION_INGESTED_DIR_NAME_KEY = "ingested_dir"
 DATA_INGESTION_TRAIN_DIR_KEY = "ingested_train_dir"
 DATA_INGESTION_TEST_DIR_KEY = "ingested_test_dir"
+
+# Data Validation related variable
+
+# Data Validation related variables
+DATA_VALIDATION_CONFIG_KEY = "data_validation_config"
+DATA_VALIDATION_SCHEMA_FILE_NAME_KEY = "schema_file_name"
+DATA_VALIDATION_SCHEMA_DIR_KEY = "schema_dir"
+DATA_VALIDATION_ARTIFACT_DIR_NAME="data_validation"
+DATA_VALIDATION_REPORT_FILE_NAME_KEY = "report_file_name"
+DATA_VALIDATION_REPORT_PAGE_FILE_NAME_KEY = "report_page_file_name"
